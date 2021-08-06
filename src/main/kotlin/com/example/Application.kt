647f4d399b7dcafddb42104a063831e6de962f96
KY <infcurious@gmail.com>
2021-08-07 05:38:25 +0800

    Setup project

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,15 @@
+package com.example
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.example.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureSecurity()
+        configureRouting()
+        configureHTTP()
+        configureMonitoring()
+        configureSerialization()
+    }.start(wait = true)
+}
---
+++ src/main/kotlin/com/example/plugins/HTTP.kt
@@ -0,0 +1,43 @@
+package com.example.plugins
+
+import io.ktor.features.*
+import io.ktor.http.content.*
+import io.ktor.http.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun Application.configureHTTP() {
+    install(CachingHeaders) {
+        options { outgoingContent ->
+            when (outgoingContent.contentType?.withoutParameters()) {
+                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60))
+                else -> null
+            }
+        }
+    }
+    install(Compression) {
+        gzip {
+            priority = 1.0
+        }
+        deflate {
+            priority = 10.0
+            minimumSize(1024) // condition
+        }
+    }
+    install(ConditionalHeaders)
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+
+}
