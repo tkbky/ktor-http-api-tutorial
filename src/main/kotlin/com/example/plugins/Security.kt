7aad31b9e4210a8cf36b2fcf9e2e88a32d577a78
KY <infcurious@gmail.com>
2021-08-07 10:42:04 +0800

    Basic auth - hashed table

--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -2,21 +2,25 @@ package com.example.plugins
 
 import io.ktor.application.*
 import io.ktor.auth.*
+import io.ktor.util.*
 
 enum class AuthProvider {
     AUTH_BASIC
 }
 
 fun Application.configureSecurity() {
+    val digestFunction = getDigestFunction("SHA-256") { "ktor-${it.length}" }
+    val hashedUserTable = UserHashedTableAuth(
+        table = mapOf(
+            "admin" to digestFunction("admin")
+        ),
+        digester = digestFunction
+    )
     install(Authentication) {
         basic(AuthProvider.AUTH_BASIC.name) {
             realm = "Access to the '/' path"
             validate { credentials ->
-                if (credentials.name == "admin" && credentials.password == "admin") {
-                    UserIdPrincipal(credentials.name)
-                } else {
-                    null
-                }
+                hashedUserTable.authenticate(credentials)
             }
         }
     }
